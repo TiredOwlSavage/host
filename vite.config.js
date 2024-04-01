3404dd90b5935217a97d5e844fee17a36d5f50c7
TiredOwlSavage <111298102+TiredOwlSavage@users.noreply.github.com>
2024-04-02 01:56:26 +0300

    deploy: accept done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/host/",
+
+
   build: {
+
     chunkSizeWarningLimit: 1000, // Увеличьте размер предупреждения чанков
     rollupOptions: {
       output: {
