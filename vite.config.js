d62f961dfba73771f4aa62cf1fa2d5a682fd873b
TiredOwlSavage <111298102+TiredOwlSavage@users.noreply.github.com>
2024-04-02 01:53:34 +0300

    deploy: accept done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/host/"
+  build: {
+    chunkSizeWarningLimit: 1000, // Увеличьте размер предупреждения чанков
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Возвращает имя пакета внутри node_modules
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
